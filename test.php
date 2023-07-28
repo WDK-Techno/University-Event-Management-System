e7a785953662de65d4c853d8f18b6941759580d2
Kavindra Weerasinghe <wdilshankavindra@gmail.com>
2023-07-28 22:50:24 +0530

    test

---
+++ test.php
@@ -0,0 +1,17 @@
+
+
+<?php
+    echo "Hello Wold!"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h5>fhsajk</h5>
+</body>
+</html>
