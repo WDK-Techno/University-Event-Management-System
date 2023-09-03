42327ac0e511f3e0fbf726fdf766ef20cbc5d694
Kavindra Weerasinghe <wdilshankavindra@gmail.com>
2023-09-03 23:57:41 +0530

    deactivate user UI

--- process/login.php
+++ process/login.php
@@ -33,6 +33,14 @@ if(isset($_POST['submit'],$_POST['username'],$_POST['password'])){
 
                     header("location: ../ug-dashboard.php");
                 }
+                if ($user->getStatus() == "deactive"){
+
+                    $ug = new Undergraduate(null, null, null, null, null, null);
+                    $ug->setUserId($user->getUserId());
+                    $ug->loadDataFromUserID($con);
+
+                    header("location: ../content/restrict.php?fname={$ug->getFirstName()}&lname={$ug->getLastName()}&typ=ug_deactive");
+                }
 
             }
             if ($user->getRole() == "club"){
