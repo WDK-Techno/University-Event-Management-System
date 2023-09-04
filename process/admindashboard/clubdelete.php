79f5df529d1f98f003d894a0436bae13ce6c5990
Kavindra Weerasinghe <wdilshankavindra@gmail.com>
2023-09-04 10:16:12 +0530

    update

--- process/admindashboard/clubdelete.php
+++ process/admindashboard/clubdelete.php
@@ -1,12 +1,11 @@
 <?php
-use classes\Admin;
 
-require_once '../../classes/admin.php';
+require_once '../../classes/Admin.php';
 require_once '../../classes/DBConnector.php';
-
+use classes\Admin;
 if (isset($_POST["user_id"])) {
     $user_id = $_POST["user_id"];
-    $userObj = new \classes\Admin();
+    $userObj = new Admin();
     $userObj->clubdelete($user_id);
     header("Location:../../superadmindashboard.php?tab=2");
 }
--- process/admindashboard/ugdelete.php
+++ process/admindashboard/ugdelete.php
@@ -1,13 +1,13 @@
 <?php
-use classes\Admin;
 
-require_once '../../classes/admin.php';
+
+require_once '../../classes/Admin.php';
 require_once '../../classes/DBConnector.php';
-
+use classes\Admin;
 if (isset($_POST["user_id"])) {
     $user_id = $_POST["user_id"];
     
-    $userObj = new \classes\Admin();
+    $userObj = new Admin();
     $userObj->ugdelete($user_id);
     header("Location:../../superadmindashboard.php?tab=1");
 }
