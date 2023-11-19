2d42308f836c58a3dbfe7da6c4f068f280c36f75
AnurangaMadushan <anurangamadushan631@gmail.com>
2023-11-19 20:44:26 +0530

    add_changes_to_sub_task_table2

--- ug-dashboard.php
+++ ug-dashboard.php
@@ -397,9 +397,7 @@ if (isset($_SESSION['user_id'])) {
 
                 </div>
                 <div id="menu-content-2" class="main-content hide">
-                    <div class="m-4">
-                        <h1>Compleated Task </h1>
-                    </div>
+
 
                 <div class="card-body pt-0 bg-dark-subtle scrollable-div Flipped"
                      style="background-color: var(--secondary);">
